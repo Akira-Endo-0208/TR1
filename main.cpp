99ed9a837bc5cfde99dc777864c1954b16103650
AKIRA_ENDO_0208 <k021g1166@g.neec.ac.jp>
2022-06-08 16:36:41 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <windows.h>
+#include <Sensapi.h>
+#include <ShlDisp.h>
+#pragma comment(lib, "Sensapi.lib")
+
+int main()
+{
+   /* BOOL isConnected;
+    DWORD kindofNetwork;
+
+
+    isConnected = IsNetworkAlive(&kindofNetwork);
+
+
+    if (isConnected == TRUE) {
+        if (kindofNetwork == NETWORK_ALIVE_LAN) {
+            printf("ネットワークに接続中\n");
+        }
+        else{
+            printf("仮想ネットワークサーバーに接続中\n");
+        }
+    }
+    else {
+        printf("ネットワーク接続はされていません。\n");
+    }*/
+
+
+    
+    ShellExecute(NULL, "open", "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Undertale\\UNDERTALE.exe", NULL, NULL, SW_SHOWNA);
+    ShellExecute(NULL, "open", "C:\\Program Files (x86)\\Steam\\steamapps\\common\\DELTARUNEdemo\\DELTARUNE.exe", NULL, NULL, SW_SHOWNA);
+    return 0;
+}
