fd05e6ce6386b9d83fea455d20aa6f0694c26aab
AKIRA_ENDO_0208 <k021g1166@g.neec.ac.jp>
2022-06-23 16:39:38 +0900

    feat:ゲームの起動

--- main.cpp
+++ main.cpp
@@ -9,29 +9,31 @@ int main()
     BOOL isConnected = 0;
     BOOL oldIsConnected = 0;
     DWORD kindofNetwork;
-    while(1)
-    { 
 
-
-    oldIsConnected = isConnected;
     isConnected = IsNetworkAlive(&kindofNetwork);
 
-
-    if (isConnected == TRUE) {
+    printf("ゲームを起動します\n");
+    ShellExecute(NULL, "open", "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Undertale\\UNDERTALE.exe", NULL, NULL, SW_SHOWNA);
+    ShellExecute(NULL, "open", "C:\\Program Files (x86)\\Steam\\steamapps\\common\\DELTARUNEdemo\\DELTARUNE.exe", NULL, NULL, SW_SHOWNA);
+    /*if (isConnected == TRUE) {
         if (kindofNetwork == NETWORK_ALIVE_LAN) {
             printf("ネットワークに接続中\n");
         }
-        else{
+        else {
             printf("仮想ネットワークサーバーに接続中\n");
         }
     }
     else {
         printf("ネットワーク接続はされていません。\n");
-    }
+    }*/
+    while (1)
+    {
 
-    
+
+        
+
+
     }
-   /* ShellExecute(NULL, "open", "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Undertale\\UNDERTALE.exe", NULL, NULL, SW_SHOWNA);
-    ShellExecute(NULL, "open", "C:\\Program Files (x86)\\Steam\\steamapps\\common\\DELTARUNEdemo\\DELTARUNE.exe", NULL, NULL, SW_SHOWNA);*/
-    return 0;
+  
+
 }
